4e261d14bb3f35884e999d00bfa5970bbea4c655
ers99 <ers16e@my.fsu.edu>
2017-02-18 17:34:34 -0500

    Added Login System

--- app/views.py
+++ app/views.py
@@ -1,19 +1,21 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
-from app import app, db, lm, old
+from app import app, db, lm, oid
 from .forms import LoginForm
 from .models import User
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-	user = {'nickname': 'Eric'}
+	user = g.user
+
 	return render_template('index.html', title = 'Home', user = user)
 
 @app.route('/database')
 def database():
-	user = {'nickname': 'Eric'}
+	user = g.user
 	return render_template('database.html', title = 'Home', user = user)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -24,9 +26,34 @@ def login():
 	form = LoginForm()
 	if form.validate_on_submit():
 		session['remember_me'] = form.remember_me.data
-		return oid.try_login(form.openid.data, ask_for=['username', 'email'])
+		return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
 	return render_template('login.html', title='Sign In', form=form, providers = app.config['OPENID_PROVIDERS'])
 
+@app.before_request
+def before_request():
+	g.user = current_user
+
+@oid.after_login
+def after_login(resp):
+	if resp.email is None or resp.email == "":
+		flash('Invalid login. Please try again.')
+		return redirect(url_for('login'))
+	user = User.query.filter_by(email=resp.email).first()
+	if user is None:
+		username = resp.nickname
+		if username is None or username == "":
+			username = resp.email.split('@')[0]
+		user = User(username=username, email=resp.email)
+		db.session.add(user)
+		db.session.commit()
+	remember_me = False
+	if 'remember_me' in session:
+		remember_me = session['remember_me']
+		session.pop('remember_me', None)
+	login_user(user, remember = remember_me)
+	return redirect(request.args.get('next') or url_for('index'))
+
+
 @lm.user_loader
-def load_user(id))
+def load_user(id):
 	return User.query.get(int(id))
